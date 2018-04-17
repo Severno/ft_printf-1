54adcb28582f443e18803f6f6e51f4fe62b88ac0
Dav HOJT <davhojt@gmail.com>
2018-04-17 14:42:40 +0200

    Moved research files to research dir.

--- research/research.c
+++ research/research.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/14 14:41:14 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/16 18:24:51 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/16 20:07:13 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,25 @@ char	s2[5] = "e";
 char	s3[5] = "\0";
 
 
-
 /* Char */
-char	c1 = 'Z';
-char	c2 = 33; /* Exclamation Mark */
-char	c3 = '\0';
-
+char	c[8] = "ZeP\tq87\0";
 
 /* Int */
-int		i1 = 2;
+int		i1 = -42;
 int		i2 = 0;
 int		i3 = 42;
 
-long long int l = 1;
-
-
-
-
-
-//unsigned
-
 int		main(void)
 {
-	//d
-	TITLE("STRINGS");
-	HEAD("Basic");
-	P("s[%i]", i1);
-	
-	P("s[%5i]", i2);
-	
-	P("s[%5i]", i3);
-	return(0);
+
+
+	test_c();
+	return (0);
+}
+
+void	test_c(void)
+{
+	int count = -1;
+	while (c[++count])
+		P("c[%c]", c[count]);
 }
--- research/research.h
+++ research/research.h
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/15 22:58:38 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/16 17:05:54 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/16 19:49:20 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,8 @@ printf(__VA_ARGS__); printf("\n\n\n");
 /*			TABULATION FORMATTING			*/
 # define TITLE(...); printf("\n\n\n\n\t\t--"); printf(__VA_ARGS__); printf("--\n");
 # define HEAD(...); printf("\n\t-"); printf(__VA_ARGS__); printf("-\n");
+
+void			test_c(void);
+
 
 #endif

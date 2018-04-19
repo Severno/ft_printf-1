9af91ff252f70cd0c9d7c9e9a15c8bf755f94545
Dav HOJT <davhojt@gmail.com>
2018-04-20 00:44:49 +0200

    Made everything unsigned

--- display_u.c
+++ display_u.c
@@ -1,46 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   display_d.c                                        :+:      :+:    :+:   */
+/*   display_u.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/17 23:44:21 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/20 00:27:31 by dhojt            ###   ########.fr       */
+/*   Created: 2018/04/20 00:41:03 by dhojt             #+#    #+#             */
+/*   Updated: 2018/04/20 00:41:17 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-intmax_t		get_num(t_DATA *DATA)
+static uintmax_t	get_num(t_DATA *DATA)
 {
-	intmax_t	num;
+	uintmax_t	num;
 	
 	if (ft_strcmp(DATA->argument_flag, "hh") == 0)
-		num = (signed char)(va_arg(DATA->args, int));
+		num = (unsigned char)(va_arg(DATA->args, unsigned int));
 	else if (ft_strcmp(DATA->argument_flag, "h") == 0)
-		num = (short)(va_arg(DATA->args, int));
+		num = (unsigned short)(va_arg(DATA->args, unsigned int));
 	else if (ft_strcmp(DATA->argument_flag, "ll") == 0)
-		num = (long long)(va_arg(DATA->args, long long int));
+		num = (unsigned long long)(va_arg(DATA->args, unsigned long long int));
 	else if (ft_strcmp(DATA->argument_flag, "l") == 0)
-		num = (long)(va_arg(DATA->args, long int));
+		num = (unsigned long)(va_arg(DATA->args, unsigned long int));
 	else if (ft_strcmp(DATA->argument_flag, "j") == 0)
-		num = (intmax_t)(va_arg(DATA->args, intmax_t));
+		num = (uintmax_t)(va_arg(DATA->args, uintmax_t));
 	else if (ft_strcmp(DATA->argument_flag, "z") == 0)
 		num = (size_t)(va_arg(DATA->args, size_t));
 	else
-		num = (int)(va_arg(DATA->args, int));
-	num = (intmax_t)num;
+		num = (unsigned int)(va_arg(DATA->args, unsigned int));
+	num = (uintmax_t)num;
 	return (num);
 }
 
 
-static int		get_tens(intmax_t num)
+static int		get_tens(uintmax_t num)
 {
 	int tens;
 
-	if (num < 0)
-		num *= -1;
 	tens = 1;
 	while ((num /= 10) > 0)
 		tens++;
@@ -61,17 +59,13 @@ static char		get_negatvity_placeholder(t_DATA *DATA, int is_negative)
 	return ('\0');
 }
 
-t_DATA			*print_d(t_DATA *DATA, intmax_t  num, int num_width, int align_left)
+static t_DATA		*print_u(t_DATA *DATA, uintmax_t  num, int num_width, int align_left)
 {
 	int			not_blank;
 	char		negatvity_placeholder;
 	int			is_negative;
 
 	is_negative = 0;
-	if (num < 0)
-		is_negative = 1;
-	if (is_negative)
-		num *= -1;
 	negatvity_placeholder = get_negatvity_placeholder(DATA, is_negative);
 	not_blank = num_width;
 	if (num_width <= DATA->precision)
@@ -84,16 +78,16 @@ t_DATA			*print_d(t_DATA *DATA, intmax_t  num, int num_width, int align_left)
 		write(1, &negatvity_placeholder, 1);
 	while (DATA->precision-- > num_width)
 		write(1, "0", 1);
-	ft_putnbrmax_fd(num, 1);
+	ft_putnbrumax_fd(num, 1);
 	while (align_left && DATA->field_width-- > not_blank)
 		write(1, " ", 1);
 	return (DATA);
 }
 
-t_DATA			*display_d(t_DATA *DATA)
+t_DATA			*display_u(t_DATA *DATA)
 {
 	char		*str;
-	intmax_t	num;
+	uintmax_t	num;
 	int			num_width;
 	int			align_left;
 
@@ -103,11 +97,7 @@ t_DATA			*display_d(t_DATA *DATA)
 	if (DATA->converter_flag[0] == '-')
 		align_left = 1;
 	if (DATA->converter_flag[3] == '0' && !DATA->precision && !DATA->converter_flag[0])
-	{
 		DATA->precision = DATA->field_width;
-		if (num < 0)
-			DATA->precision--;
-	}
-	print_d(DATA, num, num_width, align_left);
+	print_u(DATA, num, num_width, align_left);
 	return (DATA);
 }

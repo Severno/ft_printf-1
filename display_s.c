152b0ebc4e26f3a04b0e2ef7eefc75e267c6215b
Dav HOJT <davhojt@gmail.com>
2018-04-22 18:01:41 +0200

    Amended display_s to work with field width. Also, to increment DATA->len rather than to amend it.

--- display_s.c
+++ display_s.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 03:25:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/22 15:35:18 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/22 17:35:02 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,21 @@ t_DATA			*display_s(t_DATA *DATA)
 {
 	char		*s;
 	int			i;
+	int			len;
 
 	i = 0;
 	if (DATA->precision)
 		s = ft_strndup(va_arg(DATA->args, char *), DATA->precision);
 	else
 		s = ft_strdup(va_arg(DATA->args, char *));
+	len = ft_strlen(s);
+	DATA->len += len;
+	while ((DATA->field_width--) > len)
+	{
+		write (1, " ", 1);
+		DATA->len += 1;
+	}
 	ft_putstr(s);
-	DATA->len = ft_strlen(s);
 	free (s);
 	return (DATA);
 }

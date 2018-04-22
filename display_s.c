090c2aa58f2943238569b41a5fd9ba2d51861e69
Dav HOJT <davhojt@gmail.com>
2018-04-22 21:03:57 +0200

    Took care of NULL pointers. Now returns string (null);

--- display_s.c
+++ display_s.c
@@ -6,31 +6,45 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 03:25:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/22 17:35:02 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/22 19:38:35 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+static void		print_space(t_DATA *DATA, int len)
+{
+	while ((DATA->field_width--) > len)
+	{
+		write (1, " ", 1);
+		DATA->len += 1;
+	}
+}
 
 t_DATA			*display_s(t_DATA *DATA)
 {
 	char		*s;
 	int			i;
 	int			len;
+	char		*tmp;
 
 	i = 0;
-	if (DATA->precision)
-		s = ft_strndup(va_arg(DATA->args, char *), DATA->precision);
-	else
-		s = ft_strdup(va_arg(DATA->args, char *));
+	s = va_arg(DATA->args, char *);
+	if (DATA->precision && s)
+		s = ft_strndup(s, DATA->precision);
+	else if (!DATA->precision && s)
+		s = ft_strdup(s);
+	else if (DATA->precision && !s)
+		s = ft_strndup("(null)", DATA->precision);
+	else if (!DATA->precision && !s)
+		s = ft_strdup("(null)");
 	len = ft_strlen(s);
 	DATA->len += len;
-	while ((DATA->field_width--) > len)
-	{
-		write (1, " ", 1);
-		DATA->len += 1;
-	}
+	if (DATA->converter_flag[0] != '-')
+		print_space(DATA, len);
 	ft_putstr(s);
+	if (DATA->converter_flag[0] == '-')
+		print_space(DATA, len);
 	free (s);
 	return (DATA);
 }

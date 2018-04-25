3aa7608943529f03e257cdf61f846404be7166c5
Dav HOJT <davhojt@gmail.com>
2018-04-25 14:57:43 +0200

    Added switch_display

---
+++ switch_display.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   switch_display.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/25 14:56:26 by dhojt             #+#    #+#             */
+/*   Updated: 2018/04/25 14:56:55 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+t_DATA			*switch_display(t_DATA *DATA)
+{
+	char	*flag;
+
+	flag = &DATA->specifier_flag;
+	if(*flag == 'd' || *flag == 'i' || *flag == 'D')
+		display_d(DATA);
+	if(*flag == 'c')
+		display_c(DATA);
+	if(*flag == 's')
+		display_s(DATA);
+	if(*flag == 'u' || *flag == 'U')
+		display_u(DATA);
+	if(*flag == 'x' || *flag == 'X')
+		display_x(DATA);
+	if(*flag == 'o' || *flag == 'O')
+		display_o(DATA);
+	if(*flag == 'p')
+		display_p(DATA);
+	if(*flag == '%')
+		display_percent(DATA);
+	return(DATA);
+}

8a2840c17aba86b3572104e87f6791103d09211b
Dav HOJT <davhojt@gmail.com>
2018-04-25 15:44:38 +0200

    Added main for testing.

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "printf.h"
+#include <limits.h>
+
+int		main(void)
+{
+	
+	int		val;
+	unsigned int	p;
+
+	p = 4294967295;
+
+	val = ft_printf("%");
+									printf("[%d]\n\n", val);
+	val =    printf("%");
+									printf("[%d]\n\n", val);
+
+/*
+	char str[] = "yoyoyoyoyo";
+	int val;
+
+	val = ft_printf("Hello %10.0s\n", str);
+									printf("[%d]\n\n", val);
+	val =    printf("Hello %10.0s\n", str);
+									printf("[%d]\n\n", val);
+	return (0);
+	*/
+}

d46c6466883c8fc2ddd25a8e20a030a39aae4226
davhojt <davhojt@gmail.com>
2018-04-27 01:59:53 +0200

    Finished ft_putwchar. Will test and then optimise

--- srcs/libft/srcs/ft_putwchar.c
+++ srcs/libft/srcs/ft_putwchar.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 00:15:16 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/27 00:34:10 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/27 01:58:37 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,10 @@ static void		oct_4(unsigned long c)
 	unsigned char a3;
 	unsigned char a4;
 
-	printf("C=[%ld]\n", c);
 	a1 = (c >> 18) + 240;
 	a2 = ((c >> 12) & 63) + 128;
 	a3 = ((c >> 6) & 63) + 128;
 	a4 = ((c & 63) + 128);
-
-	printf("A1=[%u]\n", a1);
-	printf("A2=[%u]\n", a2);
-	printf("A3=[%u]\n", a3);
-	printf("A4=[%u]\n", a4);
 
 	write (1, &a1, 1);
 	write (1, &a2, 1);
@@ -38,12 +32,29 @@ static void		oct_4(unsigned long c)
 
 static void		oct_3(unsigned long c)
 {
-	write (1, &c, 1);
+	unsigned char a1;
+	unsigned char a2;
+	unsigned char a3;
+
+	a1 = (c >> 12) + 224;
+	a2 = ((c >> 6) & 63) + 128;
+	a3 = ((c & 63) + 128);
+
+	write (1, &a1, 1);
+	write (1, &a2, 1);
+	write (1, &a3, 1);
 }
 
 static void		oct_2(unsigned long c)
 {
-	write (1, &c, 1);
+	unsigned char a1;
+	unsigned char a2;
+
+	a1 = (c >> 6) + 192;
+	a2 = ((c & 63) + 128);
+
+	write (1, &a1, 1);
+	write (1, &a2, 1);
 }
 
 static void		oct_1(unsigned long c)
@@ -51,21 +62,8 @@ static void		oct_1(unsigned long c)
 	write (1, &c, 1);
 }
 
-int				main(void)
+void			ft_putwchar(unsigned long c)
 {
-	unsigned long c;
-
-	c = 125011;
-	
-	//c = 225;
-	//write(1, &c, 1);
-	//c = 136;
-	//write(1, &c, 1);
-	//c = 180;
-	//write(1, &c, 1);
-	//c = 10;
-	//write(1, &c, 1);
-
 	if (c <= 127)
 		oct_1(c);
 	if (c >= 127 && c <= 2047)
@@ -74,8 +72,5 @@ int				main(void)
 		oct_3(c);
 	if (c >= 65536 && c <= 2097151)
 		oct_4(c);
-	//
-	////
 	write (1, "\n", 1);
-	return (0);
 }

2604df8fa9fd641ea2a7043dcc0472d6daf59d57
Dav HOJT <davhojt@gmail.com>
2018-04-27 15:33:08 +0200

    Added display_ws

---
+++ srcs/display_ws.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_S.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/19 03:25:05 by dhojt             #+#    #+#             */
+/*   Updated: 2018/04/27 15:21:45 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <ctype.h>
+
+static wchar_t	*wstrndup(wchar_t *s1, size_t n)
+{
+	wchar_t	*s2;
+	size_t	i;
+
+	i = 0;
+	if (!(s2 = (wchar_t *)malloc(sizeof(wchar_t) * n + 1)))
+		return (NULL);
+	while (s1[i] && i < n)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	return (s2);
+}
+
+static wchar_t	*wstrdup(wchar_t *str)
+{
+	int		len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (wstrndup(str, len));
+}
+
+t_DATA			*display_ws(t_DATA *DATA)
+{
+	wchar_t		*s;
+	int			i;
+	int			len;
+
+	i = 0;
+	len = 0;
+	s = (wchar_t *)va_arg(DATA->args, wchar_t *);
+	if (DATA->precision > -1 && s)
+		s = wstrndup(s, DATA->precision);
+	else if (DATA->precision == -1 && s)
+		s = wstrdup(s);
+	else if (DATA->precision > -1 && !s)
+		s = wstrndup(L"(null)", DATA->precision);
+	else if (DATA->precision == -1&& !s)
+		s = wstrdup(L"(null)");
+	if (DATA->converter_flag[0] != '-')
+		display_gap(DATA, ' ', DATA->field_width - len, 1);
+	while (s[len])
+	{
+		ft_putwchar(s[len]);
+		DATA->len++;
+	}
+	if (DATA->converter_flag[0] == '-')
+		display_gap(DATA, ' ', DATA->field_width - len, 1);
+	free (s);
+	return (DATA);
+}

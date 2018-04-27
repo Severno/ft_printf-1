5dc65fcd3c8b9ac115b1439ee3e8233d611bb873
Dav HOJT <davhojt@gmail.com>
2018-04-27 15:27:24 +0200

    Added switch for 'S' and 'ls'

--- srcs/switch_display.c
+++ srcs/switch_display.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   display_c.c                                        :+:      :+:    :+:   */
+/*   switch_display.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/19 02:46:02 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/25 22:36:42 by dhojt            ###   ########.fr       */
+/*   Created: 2018/04/27 15:06:57 by dhojt             #+#    #+#             */
+/*   Updated: 2018/04/27 15:07:03 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ t_DATA			*switch_display(t_DATA *DATA)
 	flag = &DATA->specifier_flag;
 	if(*flag == 'd' || *flag == 'i' || *flag == 'D')
 		display_d(DATA);
-	else if(*flag == 'c')
+	else if(*flag == 'c' || *flag == 'C')
 		display_c(DATA);
+	else if(*flag == 's' && ft_strcmp(DATA->argument_flag, "ll") == 0)
+		display_ws(DATA);
 	else if(*flag == 's')
 		display_s(DATA);
+	else if(*flag == 'S')
+		display_ws(DATA);
 	else if(*flag == 'u' || *flag == 'U')
 		display_u(DATA);
 	else if(*flag == 'x' || *flag == 'X')

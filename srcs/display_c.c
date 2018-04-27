42c19b9fb09ef2466dbae11e51884d3bca1aefc2
Dav HOJT <davhojt@gmail.com>
2018-04-27 15:00:16 +0200

    Amended casting to wint_t

--- srcs/display_c.c
+++ srcs/display_c.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 02:46:02 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/27 08:53:54 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/27 11:20:08 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 
 t_DATA			*display_c(t_DATA *DATA)
 {
-	wchar_t		c;
+	wint_t		c;
 
 	if (ft_strcmp(DATA->argument_flag, "l") == 0)
 		c = (wchar_t)va_arg(DATA->args, wint_t);
 	else if (DATA->specifier_flag == 'C')
 		c = (wchar_t)va_arg(DATA->args, wint_t);
-	else //(DATA->specifier_flag == 'c')
+	else
 		c = (char)va_arg(DATA->args, int);
+	c = (wint_t)c;
 	if (DATA->converter_flag[0] != '-')
 		display_gap(DATA, ' ', DATA->field_width - 1, 1);	
 	ft_putwchar(c);

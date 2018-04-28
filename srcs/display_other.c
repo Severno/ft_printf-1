5cf9cde222784e8b327ca2f0c4785904e0b497fa
Dav HOJT <davhojt@gmail.com>
2018-04-28 20:57:46 +0200

    Corrected some precision display errors

--- srcs/display_other.c
+++ srcs/display_other.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 16:01:33 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/28 11:43:04 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/28 20:54:45 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 static t_DATA	*display_percent(t_DATA *DATA)
 {
+	if (DATA->converter_flag[3] == '0' && DATA->precision <= 0  && !DATA->converter_flag[0])
+	{
+		DATA->precision = DATA->field_width;
+		DATA->field_width = 0;
+	}
 	if (DATA->converter_flag[0] != '-')
 		display_gap(DATA, ' ', DATA->field_width - 1, 1);
+	if (DATA->converter_flag[3] == '0')
+		display_gap(DATA, '0', DATA->precision - 1, 1);
 	write(1, "%", 1);
 	if (DATA->converter_flag[0] == '-')
 		display_gap(DATA, ' ', DATA->field_width - 1, 1);
@@ -30,8 +37,15 @@ t_DATA			*display_other(t_DATA *DATA)
 		display_percent(DATA);
 		return (DATA);
 	}
+	if (DATA->converter_flag[3] == '0' && DATA->precision <= 0  && !DATA->converter_flag[0])
+	{
+		DATA->precision = DATA->field_width;
+		DATA->field_width = 0;
+	}
 	if (DATA->converter_flag[0] != '-')
 		display_gap(DATA, ' ', DATA->field_width - 1, 1);
+	if (DATA->converter_flag[3] == '0')
+		display_gap(DATA, '0', DATA->precision - 1, 1);
 	write(1, &DATA->f_copy[DATA->i], 1);
 	if (DATA->converter_flag[0] == '-')
 		display_gap(DATA, ' ', DATA->field_width - 1, 1);

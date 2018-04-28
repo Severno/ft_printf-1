19af844d5f4a7ca8a20fc9a98042b0655ae4a1b0
Dav HOJT <davhojt@gmail.com>
2018-04-28 17:05:17 +0200

    Changed len to byte len rather than char len

--- srcs/display_ws.c
+++ srcs/display_ws.c
@@ -6,12 +6,27 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 03:25:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/28 12:11:26 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/28 17:00:01 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <ctype.h>
+
+static int		char_len(wchar_t c)
+{
+	int len;
+	if (c <= 127)
+		len = 1;
+	else if (c >= 127 && c <= 2047)
+		len = 2;
+	else if (c >= 2048 && c <= 65535)
+		len = 3;
+	else
+		len = 4;
+	return (len);
+}
+
 
 static wchar_t	*wstrndup(wchar_t *s1, size_t n)
 {
@@ -43,7 +58,7 @@ static wchar_t	*wstrdup(wchar_t *str)
 t_DATA			*display_ws(t_DATA *DATA)
 {
 	wchar_t		*s;
-	int			i;
+	size_t		i;
 	int			len;
 
 	i = 0;
@@ -57,11 +72,12 @@ t_DATA			*display_ws(t_DATA *DATA)
 		s = wstrndup(L"(null)", DATA->precision);
 	else if (DATA->precision == -1&& !s)
 		s = wstrdup(L"(null)");
-	while (s[len])
-		len++;
+	while (s[i])
+		len += char_len(s[i++]);
 	if (DATA->converter_flag[0] != '-')
 		display_gap(DATA, ' ', DATA->field_width - len, 1);
-	while (len--)
+	i = 0;
+	while (s[i])
 		display_wchar(s[i++], DATA);
 	if (DATA->converter_flag[0] == '-')
 		display_gap(DATA, ' ', DATA->field_width - len, 1);

80777f1e38f480c6d4d772af301de435fb39c655
Dav HOJT <davhojt@gmail.com>
2018-04-29 13:24:40 +0200

    Normed

--- srcs/display_ws.c
+++ srcs/display_ws.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 03:25:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/29 10:05:03 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/29 13:22:17 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static wchar_t	*wstrndup(wchar_t *s1, size_t n)
 
 	i = 0;
 	if (!(s2 = (wchar_t *)malloc(sizeof(wchar_t) * n + 1)))
-		return (NULL);
+		exit(-1);
 	while (s1[i] && i < n)
 	{
 		s2[i] = s1[i];
@@ -56,28 +56,41 @@ static wchar_t	*wstrdup(wchar_t *str)
 	return (wstrndup(str, len));
 }
 
+static t_tab	*do_null(t_tab *tab)
+{
+	wchar_t		*s;
+	int			i;
+
+	i = 0;
+	s = wstrdup(L"(null)");
+	if (tab->precision > -1)
+	{
+		while (s[i] && tab->precision-- > 0)
+			display_wchar(s[i++], tab);
+	}
+	else
+	{
+		while (s[i])
+			display_wchar(s[i++], tab);
+	}
+	free(s);
+	return (tab);
+}
+
 t_tab			*display_ws(t_tab *tab)
 {
 	wchar_t		*s;
-	int			store;
 	int			i;
+	int			j;
 	int			len;
 
 	i = 0;
 	len = 0;
-	store = 0;
-	s = (wchar_t *)va_arg(tab->args, wchar_t *);
-	if (tab->precision > -1 && s)
-		s = wstrndup(s, tab->precision);
-	else if (tab->precision == -1 && s)
-		s = wstrdup(s);
-	else if (tab->precision > -1 && !s)
-		s = wstrndup(L"(null)", tab->precision);
-	else if (tab->precision == -1 && !s)
-		s = wstrdup(L"(null)");
-	while (s[i])
+	if (!(s = (wchar_t *)va_arg(tab->args, wchar_t *)))
+		return (do_null(tab));
+	while (s[i] && !(j = 0))
 	{
-		if (tab->precision > -1 && (store += char_len(s[i])) > tab->precision)
+		if (tab->precision > -1 && (len + char_len(s[i])) > tab->precision)
 			break ;
 		len += char_len(s[i++]);
 	}
@@ -85,16 +98,9 @@ t_tab			*display_ws(t_tab *tab)
 		display_gap(tab, '0', tab->field_width - len, 1);
 	else if (tab->convert[0] != '-')
 		display_gap(tab, ' ', tab->field_width - len, 1);
-	i = 0;
-	store = 0;
-	while (s[i])
-	{
-		if (tab->precision > -1 && (store += char_len(s[i])) > tab->precision)
-			break ;
-		display_wchar(s[i++], tab);
-	}
+	while (j < i)
+		display_wchar(s[j++], tab);
 	if (tab->convert[0] == '-')
 		display_gap(tab, ' ', tab->field_width - len, 1);
-	free(s);
 	return (tab);
 }
